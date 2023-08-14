4cc16fc882574d04219df741ea839bc62306e946
SwasticKumar <kswastic@gmail.com>
2023-08-14 23:31:36 +0530

    react task

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -13,11 +13,3 @@ function Header() {
 }
 
 export default Header;
-
-{/* <header className="bg-dark mb-5 py-5">
-      <div className="container px-4 px-lg-5 my-5">
-        <div className="text-center text-white">
-          <img src="/image/BG2.png" alt="banner" className="text-center" style={{ width: "250px" }} />
-        </div>
-      </div>
-    </header> */}
